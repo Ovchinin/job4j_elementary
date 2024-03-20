08f17f9646895dfa4fc55ce2567553985eb9f356
Ovchinin <ovchinin34@gmail.com>
2024-03-20 14:35:56 +0300

    4.3.6. Math.abs [#373616] Убрал переменные

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -6,7 +6,7 @@ public class ChessBoard {
         int result = 0;
         if (isValid(x1) && isValid(x2) && isValid(y1) && isValid(y2)) {
             if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
-                result = Math.abs(x2 - x1);
+                result = Math.abs(y2 - y1);
             }
         }
         return result;
